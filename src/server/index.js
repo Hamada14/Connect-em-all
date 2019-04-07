b29148f6b07282a46790e174d94dfa24477258d7
Mohamed Ayman <madoo.ayman14@gmail.com>
2019-04-07 14:50:30 +0200

    Integrate with ESLint

--- src/server/index.js
+++ src/server/index.js
@@ -6,4 +6,8 @@ const app = express();
 app.use(express.static('dist'));
 app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
 
-app.listen(process.env.PORT || 8080, () => console.log(`Listening on port ${process.env.PORT || 8080}!`));
+// eslint-disable-next-line no-console
+app.listen(process.env.PORT || 8080, () => console.log(`Listening on the port ${process.env.PORT || 8080}!`));
+
+
+    
