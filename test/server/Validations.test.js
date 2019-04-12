e19f445d68adda4b633827fd7c56c834e6c54b4e
vontman <vontman@gmail.com>
2019-04-12 22:53:01 +0200

    Added tests for backend update info

--- test/server/Validations.test.js
+++ test/server/Validations.test.js
@@ -1,17 +1,18 @@
-const userManager = require('server/UserManager'); 
+const userManager = require('server/UserManager');
 const db = require('server/database_handler');
+const bcrypt = require('bcryptjs');
 
 describe("Validate password", () => {
-    
+
     it("Validate correct password", () => {
         let password = "ThisiscorrectPassword";
-        expect(userManager.validatePassword(password, password).length).toEqual(0); 
+        expect(userManager.validatePassword(password, password).length).toEqual(0);
     });
 
     it("Validate missmatch password and confirmed password", () => {
         let password = "ThisIspassword";
         let confirmedPassword = "ThisIsMissmatchedPassword";
-        let errors = userManager.validatePassword(password, confirmedPassword); 
+        let errors = userManager.validatePassword(password, confirmedPassword);
         expect(errors.length).toEqual(1);
         expect(errors[0]).toEqual(userManager.NON_MATCHING_PASSWORD_ERROR);
     });
@@ -26,7 +27,7 @@ describe("Validate password", () => {
 })
 
 describe("Validate fullname", () => {
-    
+
     it("Validate correct full name", () => {
         let fullName = "This is correct full name";
         expect(userManager.validateName(fullName).length).toEqual(0);
@@ -111,4 +112,86 @@ describe("Validate email", () => {
         expect(errors[0]).toEqual(userManager.DUPLICATE_EMAIL_ERROR);
     })
 
+})
+
+describe("Validate new password", () => {
+
+    it("Validate correct old password", async() => {
+        let newInfo = {
+            oldPassword: "ThisisoldPassword",
+            newPassword: "ThisisnewPassword",
+            confirmPassword: "ThisisnewPassword",
+            fullName: "test",
+            birthdate: "1996-01-06"
+        };
+        let passwordSalt = bcrypt.genSaltSync(3);
+        let session = {
+            user: {
+                passwordSalt: passwordSalt,
+			    hashedPassword: bcrypt.hashSync(newInfo.oldPassword, passwordSalt)
+            }
+        };
+        let errors = await userManager.validateNewInfo(session, newInfo);
+        expect(errors.length).toEqual(0);
+    });
+
+    it("Validate incorrect old password", async() => {
+        let newInfo = {
+            oldPassword: "ThisisoldPassword",
+            newPassword: "ThisisnewPassword",
+            confirmPassword: "ThisisnewPassword",
+            fullName: "test",
+            birthdate: "1996-01-06"
+        };
+        let passwordSalt = bcrypt.genSaltSync(3);
+        let session = {
+            user: {
+                passwordSalt: passwordSalt,
+			    hashedPassword: "13245"
+            }
+        };
+        let errors = await userManager.validateNewInfo(session, newInfo);
+        expect(errors.length).toEqual(1);
+        expect(errors[0]).toEqual(userManager.WRONG_PASSWORD);
+    });
+
+    it("Validate mismatch newpassword and confirm password", async() => {
+        let newInfo = {
+            oldPassword: "ThisisoldPassword",
+            newPassword: "ThisisnewPassword",
+            confirmPassword: "ThisisnotnewPassword",
+            fullName: "test",
+            birthdate: "1996-01-06"
+        };
+        let passwordSalt = bcrypt.genSaltSync(3);
+        let session = {
+            user: {
+                passwordSalt: passwordSalt,
+			    hashedPassword: bcrypt.hashSync(newInfo.oldPassword, passwordSalt)
+            }
+        };
+        let errors = await userManager.validateNewInfo(session, newInfo);
+        expect(errors.length).toEqual(1);
+        expect(errors[0]).toEqual(userManager.NON_MATCHING_PASSWORD_ERROR);
+    });
+
+    it("Validate short new password", async() => {
+        let newInfo = {
+            oldPassword: "ThisisoldPassword",
+            newPassword: "short",
+            confirmPassword: "short",
+            fullName: "test",
+            birthdate: "1996-01-06"
+        };
+        let passwordSalt = bcrypt.genSaltSync(3);
+        let session = {
+            user: {
+                passwordSalt: passwordSalt,
+			    hashedPassword: bcrypt.hashSync(newInfo.oldPassword, passwordSalt)
+            }
+        };
+        let errors = await userManager.validateNewInfo(session, newInfo);
+        expect(errors.length).toEqual(1);
+        expect(errors[0]).toEqual(userManager.SHORT_PASSWORD_ERROR);
+    });
 })
